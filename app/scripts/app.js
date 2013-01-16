54c87fb5b835e181153487f06d324dcfbe4a1f29
Dominik Piatek <dominik.piatek@econsultancy.com>
2013-01-16 20:15:58 +0000

    Init

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var mmmoneyApp = angular.module('mmmoneyApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+mmmoneyApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
